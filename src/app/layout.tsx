72f31532d24d9d1ea967a8af143e051867c1da1a
3stasicivillian <jmark21qmc@gmail.com>
2026-02-10 08:33:26 +0800

    news

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientRoot from "./Components/ClientRoot";
-import SplashScreen from "./Components/SplashScreen";
+
 
 
 const geistSans = Geist({
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         <ClientRoot>{children}</ClientRoot>
       </body>
     </html>
